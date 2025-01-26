462119e4f6caf1d8790aeb063af793a3def0e0d0
chaitanya meshram <chaitamyameshram777@gmail.com>
2025-01-27 00:05:38 +0530

    pre deployement changes

---
+++ backend/app.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import userRouter from "./routes/userRouter.js";
+import applicationRouter from "./routes/applicationRouter.js";
+import jobRouter from "./routes/jobRouter.js";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import path from "path";
+
+
+
+const app = express();
+dotenv.config({path:"./config.env"});
+
+const _dirname = path.resolve();
+
+app.use(
+    cors({
+       origin: ['http://localhost:5173'],
+      method: ["GET", "POST", "DELETE", "PUT"],
+      credentials: true,
+    })
+  );
+
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({extended:true}));
+
+  app.use(
+    fileUpload({
+      useTempFiles: true,
+      tempFileDir: "/tmp/",
+    })
+  );
+  app.use("/api/v1/user", userRouter);
+app.use("/api/v1/job", jobRouter);
+app.use("/api/v1/application", applicationRouter);
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+})
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
